db89683f5762c426cf7aa8a531552dc03a86137c
Miradi <mengimiradit@gmail.com>
2025-12-24 21:23:31 +0100

    correction de quelques erreurs de console indexhtml

--- js/data.js
+++ js/data.js
@@ -6,7 +6,12 @@ export const data=[
         disponibilite:"disponible",
         description:"Pour présenter votre entreprise de manière professionnelle et percutante.",
         concerne:"pour PME, ASBL, Eglise, Agence, hôtel, institution, Autre",
-        li:"Design attrayant et sur mesure; Hébergement gratuit pendant 1 an; Nom de domaine gratuit pendant 1 an;Certificat SSL inclus;Assistance gratuite pendant les 2 premières semaines;Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Intégration reseaux sociaux",
+        li:`Design attrayant et sur mesure, 
+        Hébergement gratuit pendant 1 an,
+         Nom de domaine gratuit pendant 1 an,
+         Certificat SSL inclus,
+         Assistance gratuite pendant les 2 premières semaines,
+         Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Intégration reseaux sociaux`,
         duree:"3jours"
     },
     {
@@ -16,7 +21,7 @@ export const data=[
         disponibilite:"disponible",
         description:"Une plateforme complète pour refléter vos valeurs et votre expertise.",
         concerne:"pour Entreprise, ONG, etc.",
-        li:"Design attrayant et sur mesure; Hébergement gratuit pendant 1 an; Nom de domaine gratuit pendant 1 an;Certificat SSL inclus;Assistance gratuite pendant les 2 premières semaines;Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact",
+        li:"Design attrayant et sur mesure, Hébergement gratuit pendant 1 an, Nom de domaine gratuit pendant 1 an,Certificat SSL inclus,Assistance gratuite pendant les 2 premières semaines,Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact",
         duree:"7jours"
     },
     {
@@ -26,7 +31,7 @@ export const data=[
         disponibilite:"disponible",
         description:"Pour vendre vos produits/services 24h/24, avec une boutique simple à gérer.",
         concerne:"Restaurants, boutiques, etc.",
-        li:"Design attrayant et sur mesure; Hébergement gratuit pendant 1 an; Nom de domaine gratuit pendant 1 an;Certificat SSL inclus;Assistance gratuite pendant les 2 premières semaines;Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact,Référencement naturel(SEO);filtre de recherche et gestion de stock",
+        li:"Design attrayant et sur mesure, Hébergement gratuit pendant 1 an, Nom de domaine gratuit pendant 1 an,Certificat SSL inclus,Assistance gratuite pendant les 2 premières semaines,Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact,Référencement naturel(SEO),filtre de recherche et gestion de stock",
         duree:"2 semaines"
     },
     {
@@ -36,7 +41,7 @@ export const data=[
         disponibilite:"A venir",
         description:"Une application web accessible partout (même hors-ligne) avec installation sur téléphone, offrant une expérience rapide, sécurisée et digne d’une vraie application mobile + un site web complet pour booster votre visibilité en ligne.",
         concerne:"Pour entreprises qui veulent une présence mobile professionnelle : restaurants, magasins, services de livraison, agences, startups, écoles, etc.",
-        li:"Design attrayant et sur mesure; Hébergement gratuit pendant 1 an; Nom de domaine gratuit pendant 1 an;Certificat SSL inclus;Assistance gratuite pendant les 2 premières semaines;Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact,Référencement naturel(SEO);filtre de recherche et gestion de stock;Fonctionne sans connexion (hors-ligne); Installation sur smartphone",
+        li:"Design attrayant et sur mesure, Hébergement gratuit pendant 1 an, Nom de domaine gratuit pendant 1 an,Certificat SSL inclus,Assistance gratuite pendant les 2 premières semaines,Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact,Référencement naturel(SEO),filtre de recherche et gestion de stock,Fonctionne sans connexion (hors-ligne), Installation sur smartphone",
         duree:"2 à 3 semaines"
     },
     {
@@ -46,7 +51,7 @@ export const data=[
         disponibilite:"A venir",
         description:"Un logiciel complet pour gérer efficacement vos clients, produits, commandes, stocks et factures, avec un tableau de bord clair et un suivi en temps réel de votre activité.",
         concerne:"Pour PME, restaurants, boutiques, agences, associations et toutes structures souhaitant automatiser leur gestion quotidienne.",
-        li:"Design attrayant et sur mesure; Hébergement gratuit pendant 1 an; Nom de domaine gratuit pendant 1 an;Certificat SSL inclus;Assistance gratuite pendant les 2 premières semaines;Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact,Référencement naturel(SEO);filtre de recherche et gestion de stock;Fonctionne sans connexion (hors-ligne); Installation sur smartphone;Gestion des clients et fournisseurs; Gestion des produits et stocks; Suivi des commandes et factures; Reporting et statistiques en temps réel; Multi-utilisateurs avec permissions; Export PDF/Excel; Notifications pour rappels et alertes; Interface web responsive (ordinateur, tablette, téléphone); Sécurité des données; Support et assistance inclus",
+        li:"Design attrayant et sur mesure, Hébergement gratuit pendant 1 an, Nom de domaine gratuit pendant 1 an,Certificat SSL inclus;Assistance gratuite pendant les 2 premières semaines,Site responsive(Tablette, Téléphone, ordinateur),Pages, Articles,Utilisation des cookies,Formulaire de Contact,Référencement naturel(SEO),filtre de recherche et gestion de stock,Fonctionne sans connexion (hors-ligne), Installation sur smartphone,Gestion des clients et fournisseurs, Gestion des produits et stocks, Suivi des commandes et factures, Reporting et statistiques en temps réel, Multi-utilisateurs avec permissions, Export PDF/Excel, Notifications pour rappels et alertes, Interface web responsive (ordinateur, tablette, téléphone), Sécurité des données, Support et assistance inclus",
         duree:"3 à 6 semaines"
     }
 ]
--- js/index.js
+++ js/index.js
@@ -66,35 +66,37 @@ function showAlertError(title,text) {
     });
 }
 
-/*/ Fonction pour afficher les services
+// Fonction pour afficher les services*/
 const AfficherService = (services) => {
-    productContainer.innerHTML = ""; // <-- Vide le container avant d’afficher
-    if (services.length === 0) {
-        productContainer.innerHTML = `<p>Aucun service trouvé 😢</p>
-          <p class="cart-shop">🛒</p>
-        `;
-        return;
-    }
-    else{
-            services.forEach(service => {
-            const eachservice = document.createElement("div");
-            eachservice.innerHTML = generateHTML(service);
-            productContainer.appendChild(eachservice);
-        });
-    }
+  productContainer.innerHTML = ""; // <-- Vide le container avant d’afficher*/
+     if (services.length === 0) {
+       productContainer.innerHTML = `<p>Aucun service trouvé 😢</p>
+           <p class="cart-shop">🛒</p>
+         `;
+         return;
+     }
+     else{
+             services.forEach(service => {
+             const eachservice = document.createElement("div");
+             eachservice.innerHTML = generateHTML(service);
+             productContainer.appendChild(eachservice);
+         });
+     }
 
 
-};
+ };
 
-// Affiche tous les produits au chargement
-AfficherService(data);
+// Affiche tous les produits au chargement*/
+ AfficherService(data);
 
-// Filtrage
-input.addEventListener("keyup", (e) => {
-  const value = e.target.value.toLowerCase();
-  const filtre = data.filter(p =>
-    p.nom.toLowerCase().includes(value)
-  );
-  AfficherService(filtre);
-});
-*/
+// Filtrage*/
+ input.addEventListener("keyup", (e) => {
+   const value = e.target.value.toLowerCase();
+   const filtre = data.filter(p =>
+     p.nom.toLowerCase().includes(value)
+   );
+   AfficherService(filtre);
+ });
+
+
+
